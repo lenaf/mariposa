da684b26add4d829e4b0430453fcf7b7c44da79f
lenaf <lforti41@gmail.com>
2023-09-05 11:19:32 -0400

    style tweaks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Butterfly from "../components/butterfly"
 import Logo from "../components/logo"
 
 const IndexPage = () => {
@@ -17,17 +18,17 @@ const IndexPage = () => {
     <Layout>
       <SEO title="Mariposa Gallery" />
       <div className={`${colorSchemeClass} text-lg text-center h-full flex flex-col justify-center items-center`}>
-        <h1 className="font-minion mb-4">MARIPOSA</h1>
-        <div className="font-vremena mb-6">
-          <p>presents our inaugural exhibition,</p>
-          <p>Peter Schlesinger: France 1969 - 1979</p>
-          <p> opening October 15, 2023</p>
+        <h1 className="mb-4"><Logo /></h1>
+        <div className="font-vremena leading-6 mb-6">
+          <div>presents our inaugural exhibition,</div>
+          <div><i>Peter Schlesinger: France 1969 - 1979</i></div>
+          <div> opening October 15, 2023.</div>
         </div>
         <div className="font-glaston mb-4">
-          <p>50 Rue de Turenne.</p>
-          <p> Paris, 3.</p>
+          <p>50 Rue de Turenne,</p>
+          <p> Paris 3</p>
         </div>
-        <Logo />
+        <Butterfly />
       </div>
     </Layout >
   )
