a8406d167be74c3e91ebd616b40992c3c9731d4d
lenaf <lforti41@gmail.com>
2023-09-07 16:15:03 -0400

    update spacing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,13 +18,13 @@ const IndexPage = () => {
     <Layout>
       <SEO title="Mariposa Gallery" />
       <div className={`${colorSchemeClass} text-lg text-center h-full flex flex-col justify-center items-center`}>
-        <h1 className="mb-4"><Logo /></h1>
+        <h1><Logo /></h1>
         <div className="font-vremena leading-6 mb-6">
           <div>presents our inaugural exhibition,</div>
           <div><i>Peter Schlesinger: France 1969 - 1979</i></div>
           <div> opening October 15, 2023.</div>
         </div>
-        <div className="font-glaston mb-4">
+        <div className="font-glaston leading-5 mb-2">
           <p>50 Rue de Turenne,</p>
           <p> Paris 3</p>
         </div>
