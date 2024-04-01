094f96c9e27fde0e2736cf275e31ea35163d68f3
lenaf <lforti41@gmail.com>
2024-04-01 12:30:06 -0400

    remove italic

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -8,7 +8,7 @@ const Nav = () => {
         <div className="flex flex-col text-start justify-start gap-x-4.5 leading-5 font-thin">
             <Link
                 to={`/about`}
-                className={`mb-2 ${pathname === '/about' ? 'font-black italic' : ''}`}
+                className={`mb-2 ${pathname === '/about' ? 'font-black' : ''}`}
             >
                 About
             </Link>
@@ -18,19 +18,19 @@ const Nav = () => {
                 Past
             </Link>
             <Link
-                className={`ml-2 mb-2 ${pathname === '/carr' ? 'font-black italic' : ''}`}
+                className={`ml-2 mb-2 ${pathname === '/carr' ? 'font-black' : ''}`}
                 to={`/carr`}
             >
                 Drake Carr: Les Walk-ins
             </Link>
             <Link
-                className={`ml-2 mb-2 ${pathname === '/burnell' ? 'font-black italic' : ''}`}
+                className={`ml-2 mb-2 ${pathname === '/burnell' ? 'font-black' : ''}`}
                 to={`/burnell`}
             >
                 Carly Burnell: où il n’y a rien / where there is nothing
             </Link>
             <Link
-                className={`ml-2 ${pathname === '/schlesinger' ? 'font-black italic' : ''}`}
+                className={`ml-2 ${pathname === '/schlesinger' ? 'font-black' : ''}`}
                 to={`/schlesinger`}
             >
                 Peter Schlesinger: France 1969 - 1979
