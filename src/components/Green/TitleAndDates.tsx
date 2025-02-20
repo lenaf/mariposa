f24a9aceb6f3212d6f876dddaa28b47f8c2f2070
lena.forti <lena.forti@disneystreaming>
2025-02-20 16:05:46 -0500

    add hours

--- src/components/Green/TitleAndDates.tsx
+++ src/components/Green/TitleAndDates.tsx
@@ -9,7 +9,7 @@ const GreenTitleAndDates = () => {
         <span className="font-glaston">February </span>
         <span>14, 2025 - </span>
         <span className="font-glaston">February </span>
-        <span>23, 2025</span>
+        <span>23, 2025, 10-6pm</span>
       </div>
       <div>Reception: <span className="font-glaston">February </span> 18, 6-8pm </div>
       <div>526 N Western Ave, Los Angeles, California</div>
